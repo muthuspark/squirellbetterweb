6eae7a7879fdd7fa747fd0f1478bb35e56a9741b
Muthukrishnan T <muthukrishnan749@gmail.com>
2017-04-03 11:25:22 +0530

    updated for google

--- content.js
+++ content.js
@@ -23,11 +23,16 @@ if (head && head.length != 0) {
     //document.body.style['font-size'] = '18px';
     //document.body.style['line-height'] = '1.48';
     document.body.style['font-weight'] = '300';
+    if (location.hostname == "mail.google.com") {
+        // var tds = document.getElementsByTagName('td');
+        // for (var i = 0; i < tds.length; i++) { tds[i].style['font-size'] = '100%'; }
+        addNewStyle('td{font-size: 100% !important;border: none !important; padding-left:0px !important;}');
+    }
 
     //document.body.style['color'] = '#333';
-    var tags = ['p', 'td', 'input', 'h1', 'h2', 'h3', 'h4','a','span'];
+    var tags = ['p', 'td', 'input', 'h1', 'h2', 'h3', 'h4', 'a', 'span', 'button'];
     for (var j = 0; j < tags.length; j++) {
-        addNewStyle(tags[j]+' {font-family: \'Assistant\';}');
+        addNewStyle(tags[j] + ' {font-family: \'Assistant\';}');
     }
 
     addNewStyle('code span{font-family: inherit !important;}body{--x-height-multiplier: 0.35;--baseline-multiplier: 0.179;}');
